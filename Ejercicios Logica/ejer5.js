59e06c09b2bb3f82c442009072b728126773e368
karciss <cisneroscarolina869@gmail.com>
2025-03-06 10:35:08 -0400

    cambios

---
+++ Ejercicios Logica/ejer5.js
@@ -0,0 +1,18 @@
+function binario(numero) {
+    if (numero === 0) {
+        return '0';
+    }
+    
+    let binario = '';
+    while (numero > 0) {
+        binario = (numero % 2) + binario;
+        numero = Math.floor(numero / 2);
+    }
+    
+    return binario;
+    
+    // return numero.toString(2);
+}
+
+console.log(binario(10));
+console.log(binario(15));
---
+++ Ejercicios Logica/ejer6.js
@@ -0,0 +1,7 @@
+const numeros = [1,1,1,2,2,3,3,3,3];
+
+function numRepetido(array){
+    for (i=0; i<array.lenght; i++){
+       
+    }
+}
